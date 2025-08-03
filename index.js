d7431c7b774b0238ab13b69aca9f732ae7b11fd5
PheladiR <olgarachoshi@gmail.com>
2025-08-03 18:13:01 +0200

    added functionality for select button

--- index.js
+++ index.js
@@ -1,37 +1,61 @@
-//Johannesburg
-function johannesburgTime (){
-    let cityElement = document.querySelector("#Johannesburg");
-    let dateElement = cityElement.querySelector(".date");
-    let timeElement = cityElement.querySelector(".time");
+// Johannesburg
+function johannesburgTime() {
+  let johannesburgElement = document.querySelector("#Johannesburg");
+  if (johannesburgElement){
+    let dateElement = johannesburgElement.querySelector(".date");
+  let timeElement = johannesburgElement.querySelector(".time");
 
-    currentTime = moment();
-    am_pm = currentTime.format("A");
+  let currentTime = moment().tz("Africa/Johannesburg");
+  let am_pm = currentTime.format("A");
 
-    dateElement.innerHTML = currentTime
-      .tz("Africa/Johannesburg")
-      .format("MMMM Do, YYYY");
-    timeElement.innerHTML = `${currentTime
-      .tz("Africa/Johannesburg")
-      .format("hh:mm:ss")} <small>${am_pm}</small>`;
-};
+  dateElement.innerHTML = currentTime.format("MMMM Do, YYYY");
+  timeElement.innerHTML = `${currentTime.format(
+    "hh:mm:ss"
+  )} <small>${am_pm}</small>`;
+  }
+}
+setInterval(johannesburgTime, 1000);
 
-setInterval(johannesburgTime, 1000)
+// Sydney
+function sydneyTime() {
+    let sydneyElement = document.querySelector("#sydney");
+    if (sydneyElement){
+ let dateElement = sydneyElement.querySelector(".date");
+  let timeElement = sydneyElement.querySelector(".time");
 
-//Sydney
-function sydneyTime (){
-    let cityElement = document.querySelector("#sydney");
-    let dateElement = cityElement.querySelector(".date");
-    let timeElement = cityElement.querySelector(".time");
+  let currentTime = moment().tz("Australia/Sydney");
+  let am_pm = currentTime.format("A");
 
-    currentTime = moment();
-    am_pm = currentTime.format("A");
+  dateElement.innerHTML = currentTime.format("MMMM Do, YYYY");
+  timeElement.innerHTML = `${currentTime.format(
+    "hh:mm:ss"
+  )} <small>${am_pm}</small>`;
+    }
 
-    dateElement.innerHTML = currentTime
-      .tz("Australia/Sydney")
-      .format("MMMM Do, YYYY");
-    timeElement.innerHTML = `${currentTime
-      .tz("Australia/Sydney")
-      .format("hh:mm:ss")} <small>${am_pm}</small>`;
-};
+    }
+ 
+setInterval(sydneyTime, 1000);
 
-setInterval(sydneyTime, 1000)
+// Update City on dropdown change
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  let currentTime = moment().tz(cityTimezone);
+  let am_pm = currentTime.format("A");
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+
+  let cityDate = currentTime.format("MMMM Do, YYYY");
+  let cityTime = `${currentTime.format("hh:mm:ss")} <small>${am_pm}</small>`;
+
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+   <div class="city d-flex justify-content-between">
+            <div>
+                <h2 class="cityName">${cityName}</h2>
+                <div class="date  text-muted">${cityDate}</div>
+            </div>
+            <div class="time">${cityTime}</div>
+  `;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
